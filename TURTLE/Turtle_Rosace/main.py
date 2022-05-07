4e170687bfb269f107ef9224683029d173e33333
Snow <104694981+xSn0wx@users.noreply.github.com>
2022-05-07 15:47:53 +0200

    Add files via upload

---
+++ TURTLE/Turtle_Rosace/main.py
@@ -0,0 +1,14 @@
+import turtle
+# Install python3-tkinter
+
+def rosace():
+    for i in range(0, 18):
+        for i in range(0, 120):
+            t.forward(2)
+            t.left(3)
+        t.left(20)
+
+
+t = turtle.Turtle()
+rosace()
+turtle.done()
